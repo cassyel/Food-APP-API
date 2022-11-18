6522a364f26aab1e034949a529a73bd3beb97ecd
Cassyel Oliveira <cassyel1967@gmail.com>
2022-11-18 17:56:02 -0300

    Create createCategory Method On Controller Layer

---
+++ src/app/controller/categories/createCategory.ts
@@ -0,0 +1,26 @@
+import { Request, Response } from 'express';
+import Joi, { ValidationOptions } from 'joi';
+import { createCategoryService } from '../../service/categories/createCategory';
+
+const schema = Joi.object({
+  name: Joi.string().min(3).required(),
+  icon: Joi.string().min(1).required(),
+});
+
+const options: ValidationOptions = {
+  errors: {
+    wrap: {
+      label: '',
+    },
+  },
+};
+
+export async function createCategory(req: Request, res: Response) {
+  const { error } = schema.validate(req.body, options);
+  if (error) return res.status(400).json({ error: error.message });
+
+  const { name, icon } = req.body;
+  const { code, content } = await createCategoryService({ name, icon });
+
+  res.status(code).json(content);
+}
