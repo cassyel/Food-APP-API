2266e3ba9199562c01595a88aa3d7363556512ef
Cassyel Oliveira <cassyel1967@gmail.com>
2022-11-19 15:15:53 -0300

    Change Interface Names And Import Dependencies in createCategory Method

--- src/app/models/categories/createCategory.ts
+++ src/app/models/categories/createCategory.ts
@@ -1,16 +1,16 @@
 import { Category } from '../../database/Category';
 
-export interface categoryProps {
+export interface ICategoryProps {
   name: string;
   icon: string
 }
 
-export async function createCategoryModel({ name, icon }: categoryProps) {
+export async function createCategoryModel({ name, icon }: ICategoryProps) {
   const newCategory = await Category.create({ name, icon });
   return newCategory;
 }
 
-export async function findCategoryModel({ name, icon }: categoryProps) {
+export async function findCategoryModel({ name, icon }: ICategoryProps) {
   const category = await Category.findOne({ name, icon });
   return category;
 }
--- src/app/service/categories/createCategory.ts
+++ src/app/service/categories/createCategory.ts
@@ -1,7 +1,6 @@
-import { createCategoryModel, findCategoryModel } from '../../models/categories/createCategory';
-import { categoryProps } from '../../models/categories/createCategory';
+import { createCategoryModel, findCategoryModel, ICategoryProps } from '../../models/categories/createCategory';
 
-export async function createCategoryService({ name, icon }: categoryProps) {
+export async function createCategoryService({ name, icon }: ICategoryProps) {
   const existsCategory = await findCategoryModel({ name, icon });
 
   if (existsCategory) return { content: { error: 'Category already exists' }, code: 400 };
