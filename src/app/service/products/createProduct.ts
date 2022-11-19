55790991532d97a4344896b31cdf1d4aa7a81813
Cassyel Oliveira <cassyel1967@gmail.com>
2022-11-19 16:06:05 -0300

    Create createProduct Service Layer

---
+++ src/app/service/products/createProduct.ts
@@ -0,0 +1,13 @@
+import { createProductModel, findProductModel, IProductProps } from '../../models/products/createProduct';
+
+export async function createProductService({ name, description, imagePath, price, ingredients, category }:IProductProps) {
+  const existsProduct = await findProductModel({ name });
+
+  if (existsProduct) return { content: { error: 'Product already exists' }, code: 400 };
+
+  const newProduct = await createProductModel({ name, description, imagePath, price, ingredients, category });
+
+  return newProduct
+    ? { content: newProduct, code: 201 }
+    : { content: { error: 'Category not created' }, code: 400 };
+}
