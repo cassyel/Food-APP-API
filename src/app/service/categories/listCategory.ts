2f8a6ea45ed3eb09cf9888d34e4d78c8ead55b21
Cassyel Oliveira <cassyel1967@gmail.com>
2022-11-19 12:31:36 -0300

    Change listCategoriesService Rules

--- src/app/service/categories/listCategory.ts
+++ src/app/service/categories/listCategory.ts
@@ -3,8 +3,7 @@ import { listCategoriesModel } from '../../models/categories/listCategories';
 export async function listCategoriesService() {
   const allCategories = await listCategoriesModel();
 
-  return allCategories
+  return allCategories.length !== 0
     ? { content: allCategories, code: 200 }
     : { content: { error: 'No category registered' }, code: 400 };
-
 }
