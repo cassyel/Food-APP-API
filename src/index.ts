bb5b870db1e6249eccdc0b11f65772337779d21c
Cassyel Oliveira <cassyel1967@gmail.com>
2022-11-20 23:20:04 -0300

    Add Static Method Route Uploads Images

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import path from 'node:path';
 import express from 'express';
 import mongoose from 'mongoose';
 import { router } from './router';
@@ -23,6 +24,7 @@ mongoose.connect('mongodb://localhost:27017/Waiter-APP', {
 
     app.use(express.json());
     app.use(router);
+    app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
     app.use(serverError);
 
   })
