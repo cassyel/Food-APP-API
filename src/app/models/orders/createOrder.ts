93c786f6c53a6464bbf60a50e8bdc0a4545da1c0
Cassyel Oliveira <cassyel1967@gmail.com>
2022-12-05 00:45:16 -0300

    Create findOrder Model Layer and change IOrderProps Interface

--- src/app/models/orders/createOrder.ts
+++ src/app/models/orders/createOrder.ts
@@ -1,12 +1,24 @@
 import { Order } from '../../database/Order';
-import { IProductProps } from '../products/createProduct';
+
+type orderProduct = {
+  product: string;
+  quantity: number;
+}
 
 export interface IOrderProps {
   table: string;
-  products: IProductProps;
+  products: orderProduct[];
 }
 
 export async function createOrderModel({ table, products }: IOrderProps) {
   const newOrder = await Order.create({ table, products });
   return newOrder;
 }
+
+export async function findOrderModel({ table }: Partial<IOrderProps>) {
+  const status = Order.schema.paths.status.options.default;
+
+  const order = await Order.find({ table, status });
+  return order;
+}
+
