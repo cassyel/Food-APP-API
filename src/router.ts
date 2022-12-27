ddb574c0bc536336fb3c077a14885c697a972d61
Cassyel Oliveira <cassyel1967@gmail.com>
2022-12-26 23:56:48 -0300

    Add Underscore on unused parameters

--- src/router.ts
+++ src/router.ts
@@ -18,10 +18,10 @@ export const router = Router();
 
 const upload = multer({
   storage: multer.diskStorage({
-    destination(req, file, callback) {
+    destination(_req, _file, callback) {
       callback(null, path.resolve(__dirname, '..', 'uploads'));
     },
-    filename(req, file, callback) {
+    filename(_req, file, callback) {
       callback(null, `${Date.now()}-${file.originalname}`);
     },
   }),
