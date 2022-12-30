308d34e7e2ce11f37948f75b8607d3ff16b3d109
Cassyel Oliveira <cassyel1967@gmail.com>
2022-12-30 11:21:02 -0300

    Change Rules For Login

--- src/app/service/users/login.ts
+++ src/app/service/users/login.ts
@@ -6,9 +6,7 @@ import jwt from 'jsonwebtoken';
 export async function loginService({ name, email, password }: IUserProps) {
   const user = await loginModel({ name, email });
 
-  if(!user) return  { content: { error: 'Not foud user' }, code: 400 };
-
   return user && bcrypt.compareSync(password,  user.password)
     ? { content: { token: jwt.sign({ name, email, password }, String(process.env.SECRET)) }, code: 200 }
-    : { content: { error: 'Invalid Password' }, code: 400 };
+    : { content: { error: 'Login Error' }, code: 400 };
 }
