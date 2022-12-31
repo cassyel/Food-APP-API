1c5bcd0fc0ce24f8c77c079980f3e79aba64a111
Cassyel Oliveira <cassyel1967@gmail.com>
2022-12-31 01:46:51 -0300

    Fix JWT Accept All Tokens Error

--- src/authMiddleware.ts
+++ src/authMiddleware.ts
@@ -4,10 +4,10 @@ import jwt from 'jsonwebtoken';
 export async function authMiddleware(req: Request, res: Response, next: NextFunction) {
   const { authentication } = req.headers;
 
-  jwt.verify(
-    String(authentication),
-    String(process.env.SECRET),
-    (error, result) => error ? res.status(400).json({ error }) : res.locals.authentication = result);
-
-  next();
+  try {
+    jwt.verify(String(authentication),String(process.env.SECRET));
+  } catch {
+    return res.status(403).json({ error: 'JWT Required' });
+  }
+  return next();
 }
