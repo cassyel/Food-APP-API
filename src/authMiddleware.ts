282eab1909246ab5bb85b43c32639dc95ad391c9
Cassyel Oliveira <cassyel1967@gmail.com>
2023-01-03 19:28:07 -0300

    Change authMiddleware Method Verify

--- src/authMiddleware.ts
+++ src/authMiddleware.ts
@@ -1,12 +1,21 @@
 import { Request, Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
 
-export async function authMiddleware(req: Request, res: Response, next: NextFunction) {
-  const { authentication } = req.headers;
+export async function authMiddleware(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  const { authorization } = req.headers;
 
   try {
-    jwt.verify(String(authentication),String(process.env.SECRET));
-  } catch {
+    if (!authorization?.startsWith('Bearer ')) throw new Error();
+
+    jwt.verify(
+      String(authorization?.substring(7, authorization.length)),
+      String(process.env.SECRET)
+    );
+  } catch (err) {
     return res.status(403).json({ error: 'JWT Required' });
   }
   return next();
