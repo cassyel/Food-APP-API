315dd879f3b847836120e96b75860d0f494afb26
Cassyel Oliveira <cassyel1967@gmail.com>
2023-01-12 02:34:47 -0300

    Create ProductID validation on createOrder

--- src/app/service/orders/createOrder.ts
+++ src/app/service/orders/createOrder.ts
@@ -1,6 +1,16 @@
 import { createOrderModel, findOrderModel, ICreateOrderProps } from '../../models/orders/createOrder';
+import { listProductById } from '../../models/products/listProductById';
+import { isValidObjectId } from 'mongoose';
 
 export async function createOrderService({ table, products }: ICreateOrderProps) {
+  if (!products.every((item) => isValidObjectId(item.product)))
+    return { content: { error: 'Invalid productID' }, code: 400 };
+
+  const findProductsByID = await listProductById({ productsIds: products.map(item => item.product) });
+
+  if(findProductsByID.length === 0 || findProductsByID.length !== products.length)
+    return { content: { error: 'Some product informed not exists or is duplicatted' }, code: 400 };
+
   const registeredOrders = await findOrderModel({ table });
 
   const filteredOrders = registeredOrders.filter(
