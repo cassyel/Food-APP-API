bb873565498bf3f2d7f1e38a3ed4b2612365c77e
Cassyel Oliveira <cassyel1967@gmail.com>
2023-01-24 01:21:15 -0300

    Add Error message

--- src/errorHandling.ts
+++ src/errorHandling.ts
@@ -1,6 +1,6 @@
 import { Response, Request, NextFunction } from 'express';
 
-export function serverError(_err: Error, _req: Request, res: Response, next: NextFunction) {
-  res.status(500).json({ error: 'Server Error' });
+export function serverError(err: Error, _req: Request, res: Response, next: NextFunction) {
+  res.status(500).json({ error: err.message });
   next();
 }
