deb38af43d7b67add71ec9ef48fda03c36f265ab
cassyel <cassyel1967@gmail.com>
2023-03-01 16:44:39 -0300

    Cors

--- src/index.ts
+++ src/index.ts
@@ -23,6 +23,11 @@ mongoose
 
     app.use(express.json());
     app.use(cors());
+    app.use(function(req, res, next) {
+      res.header('Access-Control-Allow-Origin', 'https://food-app-api-production.up.railway.app/'); // update to match the domain you will make the request from
+      res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+      next();
+    });
     app.use(router);
     app.use('/uploads/images', express.static(path.resolve(__dirname, '..', 'uploads')));
     app.use('/', swaggerUi.serve, swaggerUi.setup(swagerDocs));
